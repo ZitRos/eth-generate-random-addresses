b795339b8d2d4aa7c68437940182d04fe89592ec
ZitRo <zitros.lab@gmail.com>
2018-06-19 16:06:24 +0300

    Upload script

---
+++ gen.js
@@ -0,0 +1,29 @@
+const generate = 2249; // Number of addresses to generate
+const decimals = 6; // Number of decimals (not more than 6!)
+let totalTokens = 30957882.3626; // Total amount to generate
+
+totalTokens *= Math.pow(10, decimals);
+const floor = (x) => Math.floor(Math.pow(10, decimals) * x) / Math.pow(10, decimals);
+const median = Math.floor(totalTokens / generate);
+const rest = totalTokens - median * generate;
+const random = (x) => Math.floor(Math.random() * x);
+const randomAddress = () => `0x${ Array.from({ length: 40 }, () => "0123456789abcdef"[random(16)]).join("") }`;
+const array = Array.from({ length: generate }, () => [randomAddress(), median] );
+array[array.length - 1][1] += rest;
+Array.from({ length: generate }).forEach(() => {
+  const a = random(generate); const b = random(generate);
+  const v = Math.max(0, Math.floor(Math.min(array[a][1], array[b][1]) / (3 - Math.random())));
+  array[a][1] -= v;
+  array[b][1] += v;
+});
+//array.forEach((a, i) => array[i][1] /= Math.pow(10, decimals));
+//totalTokens /= Math.pow(10, decimals);
+console.log(array.map(x => {
+  const a = x.slice(); a[1] = a[1].toString();
+  const e1 = a[1].slice(0, -6);
+  const e2 = a[1].slice(-6);
+  a[1] = (e1 || "0") + "." + (e2.length < 6 ? Array.from({ length: 6 - e2.length }, () => "0").join("") + e2 : e2);
+  return a.join("\t");
+}).join("\n"));
+const sum = array.map(x => x[1]).reduce((a, b) => a + b, 0);
+console.log(sum, totalTokens, sum === totalTokens);
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Script to Generate Random Ethereum Addresses
+
+...and spread the given `balance` over these addresses. See the script for editable parameters.
+
+## Example
+
+```bash
+node gen.js
+```
+
+```txt
+0xdef609f36ed18e348908099127387561d4a60b47      19966.816566
+0xcf33534097bfdd1297f13a8588181cfa5b2b0fee      19718.216502
+0xe33edaef0ad58ced297f7d582c0d598774f390f4      24804.079735
+0x93894bcb144b28469eac52564bbe5af45fb178bc      7036.040948
+0x9e906d1389c7a4a33942bad3c27f79f67b0bca1f      13765.176684
+0xd5071f68b794abb970ef2a9d47aa89f6abbc192b      5994.851224
+...
+```
+
+## License
+
+[MIT](LICENSE) (c) [Nikita Savchenko](https://nikita.tk)
